309e423d65ce82d2b31cd039b8683761a4f7c332
Felix Ayala <felix1262@gmail.com>
2016-08-31 16:38:19 -0400

    Initial commit

---
+++ AFNetworking-JSONModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetworking-JSONModel
+//
+//  Created by Felix Ayala on 8/31/16.
+//  Copyright © 2016 Felix Ayala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
